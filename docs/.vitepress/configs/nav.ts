644a2a1ec5cf17c0e8dc0f2a4a4330e4bfca6eab
z1508850 <73526298+z1508850@users.noreply.github.com>
2024-06-11 19:22:44 +0800

    Update nav.ts

--- docs/.vitepress/configs/nav.ts
+++ docs/.vitepress/configs/nav.ts
@@ -1,15 +1,12 @@
 import type { DefaultTheme } from 'vitepress'
 
 export const nav: DefaultTheme.Config['nav'] = [
-  { text: '前端导航', link: '/nav/' },
-  { text: '茂茂主页', link: 'https://fe-mm.com' },
+  { text: '主页', link: '/nav/' },
+  { text: 'GS数据库', link: 'https://lschampions.netlify.app/' },
   {
-    text: '茂茂物语',
-    link: 'https://notes.fe-mm.com',
+    text: 'BOT数据库',
+    link: 'https://badspacebar.github.io/sdk/index.html',
   },
-  { text: 'mmPlayer', link: 'https://netease-music.fe-mm.com' },
-  {
-    text: '油猴脚本',
-    link: 'https://github.com/maomao1996/tampermonkey-scripts',
-  },
+  { text: 'BGX数据库', link: 'https://bgxdb.com/' },
+  
 ]
